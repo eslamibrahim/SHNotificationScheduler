a971f55a9528e961fa81821c6c5b6cf887bbe336
islam <e-i.awwad@lean.sa>
2022-07-20 11:19:44 +0300

    Initial Commit

---
+++ SHNotificationScheduler/SHNotificationScheduler.h
@@ -0,0 +1,18 @@
+//
+//  SHNotificationScheduler.h
+//  SHNotificationScheduler
+//
+//  Created by islam on 20/07/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SHNotificationScheduler.
+FOUNDATION_EXPORT double SHNotificationSchedulerVersionNumber;
+
+//! Project version string for SHNotificationScheduler.
+FOUNDATION_EXPORT const unsigned char SHNotificationSchedulerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHNotificationScheduler/PublicHeader.h>
+
+
